1e5d75e9a731be01479e0c042370fe3a033a3963
Santiago Villagomez <santiago.fvc@gmail.com>
2015-05-16 18:44:20 +1000

    add missing file to support the max pollution value query

--- HTTPConnection.py
+++ HTTPConnection.py
@@ -2,6 +2,7 @@ __author__ = 'santiago'
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import os
 from MainProgram import main_program
+from MainProgram import get_max_pollution_value
 import time
 import urlparse
 # from urlparse import urlparse
@@ -16,10 +17,18 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # print(self.path)
         my_parser = ParamParser()
-        start,end = my_parser.parse_url_params(self.path)
-        route = main_program(start,end)
-        my_encoder = route_json_encoder()
-        coord_json = my_encoder.encode(route)
+        if 'start=' in self.path:
+            start,end = my_parser.parse_url_params(self.path)
+            route = main_program(start,end)
+            my_encoder = route_json_encoder()
+            coord_json = my_encoder.encode(route)
+            self.send_response(200,coord_json)
+        elif 'maxValue=' in self.path:
+            ret_val = get_max_pollution_value()
+            my_encoder = route_json_encoder()
+            max_val_json = my_encoder.encode(ret_val)
+            self.send_response(200,max_val_json)
+
 
         # print(myurlparse.parse_qs(os.environ['']))
         # print("CAlled from:",self.path())
@@ -27,7 +36,7 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
         # my_encoder = route_json_encoder()
         # coord_json = my_encoder.encode(route)
         # self.send_header()
-        self.send_response(200,coord_json)
+        # self.send_response(200,coord_json)
         # self.send_header('Content type','text-html')
         # self.end_headers()
         # self.wfile.write(coord_json)
