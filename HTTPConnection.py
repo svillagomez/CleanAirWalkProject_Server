365e94c0b1b3d616dc3d2c4acb8f816d044d6144
Santiago Villagomez <santiago.fvc@gmail.com>
2015-06-08 11:48:08 +1000

    added some comments

--- HTTPConnection.py
+++ HTTPConnection.py
@@ -9,7 +9,16 @@ import json
 
 # suponiendo = "/q?lat-37.7999168567061lon144.93830289691687lat-37.80298775026495lon144.94625229388475"
 
+
+# Name = HttpRequestHandler: class
+# Usage: define method to encode routes into JSON
+# Costructor parameters: BaseHTTPRequestHandler => HTTP handler
 class HttpRequestHandler(BaseHTTPRequestHandler):
+
+    # function: do_GET
+    # description: processes get requests (update the DB after every call
+    # return: None
+    # parameters: None
     def do_GET(self):
         my_parser = ParamParser()
         if 'start=' in self.path:
@@ -27,12 +36,19 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
         update()
 
     def log_message(self, format, *args):
-        return 
+        return
 
+
+# Name = Route_json_encoder: class
+# Usage: define method to encode routes into JSON
 class Route_json_encoder():
     def __init__(self):
         pass
 
+    # function: _encode
+    # description: encode a route into json format
+    # return: data_string =>  json encoding of a route
+    # parameters: route
     def _encode(self, route):
         route_dict = {}
 
@@ -42,10 +58,17 @@ class Route_json_encoder():
         data_string = json.dumps(route_dict)
         return data_string
 
+# Name = ParamParser: class
+# Usage: define method to parse parse parameters
 class ParamParser():
     def __init__(self):
         pass
 
+    # function: parse_url_params
+    # description: parse parameters ( get start and end coordinates)
+    # return: start =>  start coordinates (lat , lon)
+    #           end => start coordinates (lat , lon)
+    # parameters: the incoming request to this server
     def parse_url_params(self, path):
         if '?' in path:
             path,tmp = path.split('?', 1)
@@ -57,6 +80,10 @@ class ParamParser():
         return (start,end)
 
 
+# function: run
+# description: runs the main logic of server (run server)
+# return: None
+# parameters: None
 def run():
     HOST_NAME = ""
     PORT = 5678
